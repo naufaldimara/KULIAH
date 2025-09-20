dd2e3046b1016f38df7a1b2decda464870055aae
Noufal Dimara <noufal123@gmail.com>
2025-09-20 16:15:37 -0700

    Perubahan terbaru: hapus tugas1 + update gaji karyawan

---
+++ LATIHAN_DISEKOLAH/main2.dart
@@ -0,0 +1,12 @@
+
+void main () {
+  String nama="Naufal";
+  int umur=19;
+  double tinggi=195.2;
+  var status="Rektor";
+
+  print("nama:$nama");
+  print("Umur:$umur");
+  print("tinggi:$tinggi");
+  print("status:$status");
+}
---
+++ LATIHAN_DISEKOLAH/main3.dart
@@ -0,0 +1,18 @@
+void main () {
+  int a= 10;
+  double b= 20;
+  bool apakahaktif=false;
+
+ // print(a.runtimeType);
+
+  Set<String> user={'naufal','Bayu'};
+  dynamic nama=['naufal','Bayu','rikos'];
+  dynamic map= {
+    "Naufal": 90,
+    "Bayu": 30,
+    "Riko": 98,
+    "Bool":true
+  };
+  print(map);
+
+}
